14cbeff10962bf387341d075983d5db3f468b49a
chintya dewi <chintyadewi@chintya.local>
2020-12-17 20:42:17 +0800

    login

---
+++ login.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+$koneksi = new mysqli("localhost", "root","","bantenku");
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>Login Pelanggan</title>
+</head>
+<body>
+    
+</body>
+</html>
