634978cd4bc52186dc6e2c280545ab3dad6b37d9
chintya dewi <chintyadewi@chintya.local>
2020-12-17 18:10:24 +0800

    halaman keranjang selesai

---
+++ keranjang.php
@@ -0,0 +1,70 @@
+ <?php
+    session_start();
+
+  
+    $koneksi=new mysqli("localhost", "root","","bantenku");
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Keranjang Belanja</title>
+    <link rel="stylesheet" href="admin/assets/css/bootstrap.css">
+</head>
+<body>
+    <!-- navigation bar -->
+<nav class="navbar navbar-default">
+    <div class="container">
+        <ul class="nav navbar-nav">
+            <li><a href="index.php">Beranda</a></li>
+            <li><a href="keranjang.php">Keranjang</a></li>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="checkout.php">Checkout</a></li>
+        </ul>
+    </div>
+</nav>
+
+<section class="konten">
+    <div class="container">
+        <h1>Keranjang Belanja</h1>
+        <hr>
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>No.</th>
+                    <th>Banten</th>
+                    <th>Harga</th>
+                    <th>Jumlah</th>
+                    <th>Subharga</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php $nomor=1; ?>
+                <?php foreach ($_SESSION["keranjang"] as $id_produk => $jumlah): ?>
+                <?php
+                    $ambil= $koneksi->query("SELECT*FROM produk
+                    WHERE id_produk='$id_produk'");
+                    $pecah = $ambil->fetch_assoc();
+                    $subharga = $pecah["harga_produk"]*$jumlah;
+                ?>
+                <tr>
+                    <td><?php echo $nomor; ?></td>
+                    <td><?php echo $pecah['nama_produk'];?></td>
+                    <td>Rp.<?php echo number_format($pecah["harga_produk"]);?></td>
+                    <td><?php echo $jumlah; ?></td>
+                    <td>Rp.<?php echo number_format($subharga);?></td>
+                </tr>
+                <?php $nomor++ ?>
+                <?php endforeach ?>
+            </tbody>
+        </table>
+
+            <a href="index.php" class="btn btn-default">Belanja Lagi</a>
+            <a href="checkout.php" class="btn btn-primary">Checkout</a>
+
+    </div>
+</section>
+</body>
+</html>
